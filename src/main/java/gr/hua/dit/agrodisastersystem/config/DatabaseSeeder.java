65e20b96a61fd12fcae76b475d37db5a4f863e8b
Chris Maikos <chmaikos@gmail.com>
2023-12-20 20:35:51 +0200

    Create database seeder for initial test users

---
+++ src/main/java/gr/hua/dit/agrodisastersystem/config/DatabaseSeeder.java
@@ -0,0 +1,40 @@
+package gr.hua.dit.agrodisastersystem.config;
+
+import gr.hua.dit.agrodisastersystem.model.User;
+import gr.hua.dit.agrodisastersystem.repository.UserRepository;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Component;
+
+@Component
+public class DatabaseSeeder implements CommandLineRunner {
+
+    private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
+
+    public DatabaseSeeder(UserRepository userRepository, PasswordEncoder passwordEncoder) {
+        this.userRepository = userRepository;
+        this.passwordEncoder = passwordEncoder;
+    }
+
+    @Override
+    public void run(String... args) {
+        createUserIfNotFound("219113", "admin", "admin", "yourAdminPassword", "ADMIN", "ADMIN@gmail.com");
+        createUserIfNotFound("165905546", "thanos", "chourlias", "yourFarmerPassword", "FARMER", "farmer@gmail.com");
+    }
+
+    private void createUserIfNotFound(String tinNumber, String firstName, String lastName, String password, String role, String email) {
+        User userOptional = userRepository.findByTinNumber(tinNumber);
+
+        if (userOptional==null) {
+            User user = new User();
+            user.setTin_number(tinNumber);
+            user.setFirstName(firstName);
+            user.setLastName(lastName);
+            user.setPassword(passwordEncoder.encode(password));
+            user.setRole(role);
+            user.setEmail(email);
+            userRepository.save(user);
+        }
+    }
+}
